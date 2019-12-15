a14fc716aae7ffebab9889162845e1ba1f941d55
Wan-Jun-H <39490562+Wan-Jun-H@users.noreply.github.com>
2019-12-15 16:12:51 +0800

    第一次提交

---
+++ src/testGit/testMyGit.java
@@ -0,0 +1,21 @@
+package testGit;
+
+public class testMyGit {
+
+	public testMyGit() {
+		
+	}
+	public void test1() {
+		System.out.println("主线版本");
+	}
+	
+	public static void main(String[] args) {
+		
+		testMyGit testMyGit = new testMyGit();
+		
+		testMyGit.test1();
+	}
+
+}
+
+
